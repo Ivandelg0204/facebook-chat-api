f11e9122224fb96388d18a0cb603082c62a4c4dc
Ivandelg0204 <60906108+Ivandelg0204@users.noreply.github.com>
2026-03-09 11:28:25 -0500

    Implement Facebook Messenger bot functionality

---
+++ bot.js
@@ -0,0 +1,27 @@
+const login = require("facebook-chat-api");
+
+login(
+  { email: "ivandelg024@gmail.com", password: "3144267821IvanDelgado" },
+  (err, api) => {
+    if (err) return console.error(err);
+
+    console.log("Messenger conectado");
+
+    api.listenMqtt((err, message) => {
+
+      if (err) return console.error(err);
+
+      if (message.body) {
+
+        console.log("Mensaje:", message.body);
+
+        api.sendMessage(
+          "Hola, soy Marco si esta disponible.",
+          message.threadID
+        );
+
+      }
+
+    });
+  }
+);
